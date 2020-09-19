ecbdbdc406bdfcc0a4e365900d6f19a390fa4c76
Osman Cihan Kilinc <okilinc@ucsd.edu>
2020-09-19 15:11:29 -0700

    update 9.palindrome-number.cpp

--- 9.palindrome-number.cpp
+++ 9.palindrome-number.cpp
@@ -1,3 +1,15 @@
+/* 
+ * Cases: 
+ *  if negative -> false
+ *  if divisible by 10 -> false
+ * Edge cases:
+ *  if 0 -> true
+ *
+ * Sol:
+ *  Reverse the number. (Same as 7 - "Reverse Integer")
+ *      if it's equal to itself -> true
+ *      if it's NOT equal to itself -> false
+ * */
 class Solution {
 public:
     bool isPalindrome(int x) {
