e9bdb94c0ff93fd2d78e32e71e5f9e8b17596f88
Osman Cihan Kilinc <okilinc@ucsd.edu>
2020-10-24 16:37:56 -0700

    update 682.baseball-game.cpp

--- 682.baseball-game.cpp
+++ 682.baseball-game.cpp
@@ -6,7 +6,6 @@ public:
        int o = 0;
        std::vector<int> sc; 
        for(std::vector<std::string>::iterator it = ops.begin(); it != ops.end(); it++){
-           std::cout << *it << std::endl;
             if(*it == "D") sc.push_back(sc.back()*2);
             else if(*it == "C") sc.pop_back();
             else if(*it == "+") sc.push_back(sc[sc.size()-1] + sc[sc.size()-2]);
